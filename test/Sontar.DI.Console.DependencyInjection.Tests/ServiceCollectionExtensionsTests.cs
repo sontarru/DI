f3d94d1504659896ca90ef962ff7e7309fd019c0
fkthat <fkthat@yandex.ru>
2025-07-04 09:11:26 +0300

    Add Console libs.

---
+++ test/Sontar.DI.Console.DependencyInjection.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using AwesomeAssertions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sontar.DI;
+
+public class ServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddConsole_Registers_proper_services()
+    {
+        ServiceCollection services = new();
+
+        services.AddConsole();
+
+        services.Should().Contain(sd =>
+            sd.ServiceType == typeof(IConsoleText) &&
+            sd.ImplementationType == typeof(ConsoleAdapter) &&
+            sd.Lifetime == ServiceLifetime.Transient);
+
+        services.Should().Contain(sd =>
+            sd.ServiceType == typeof(IConsoleBinary) &&
+            sd.ImplementationType == typeof(ConsoleAdapter) &&
+            sd.Lifetime == ServiceLifetime.Transient);
+
+        services.Should().Contain(sd =>
+            sd.ServiceType == typeof(IConsoleKeyboard) &&
+            sd.ImplementationType == typeof(ConsoleAdapter) &&
+            sd.Lifetime == ServiceLifetime.Transient);
+    }
+}
